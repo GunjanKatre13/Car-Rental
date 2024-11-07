96b428b8db2348f94bb42a32e7be2def2565eab2
GunjanKatre13 <course.gk945@gmail.com>
2024-11-07 15:38:40 +0530

    Initial Commit

---
+++ backend/db.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+
+function connectDB(){
+// mongoose.connect('mongodb+srv://amine:marco123@miniproject.6qhyg5p.mongodb.net/car_rental', {useUnifiedTopology: true, useNewUrlParser:true}) 
+mongoose.connect('mongodb+srv://katregunjan21:GunjanID$$9876@cluster0.d5io4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {useUnifiedTopology: true, useNewUrlParser:true}) 
+
+const connection = mongoose.connection
+connection.on('connected', ()=>{
+    console.log('Mongo DB Connection Successfull')
+})
+    connection.on('error', ()=>{
+    console.log('Mongo DB Connection Error')
+    })
+}
+connectDB()
+module.exports = mongoose
